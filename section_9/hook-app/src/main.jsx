76dfed2279e5b137ecbc5c4a091acf81a5a488ca
jtrejos8 <jtrejos8.col@gmail.com>
2022-08-19 16:10:19 -0500

    add memos and homework

--- section_9/hook-app/src/main.jsx
+++ section_9/hook-app/src/main.jsx
@@ -5,13 +5,25 @@ import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 import { SimpleForm } from './02-useEffect/SimpleForm';
 import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
+import { FocusScreen } from './04-useRef/FocusScreen';
+import { Layout } from './05-useLayoutEffect/Layout';
+import { CallbackHook } from './06-memos/CallbackHook';
+import { MemoHook } from './06-memos/MemoHook';
+import { Memorize } from './06-memos/Memorize';
+import { Padre } from './07-tarea-memo/Padre';
 import { HooksApp } from './HooksApp';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <SimpleForm />
   // <FormWithCustomHook />
-  <MultipleCustomHooks />
+  // <MultipleCustomHooks />
+  // <FocusScreen />
+  // <Layout />
+  // <Memorize />
+  // <MemoHook />
+  // <CallbackHook />
+  <Padre />
   // <React.StrictMode>
   //    <HooksApp /> 
   //   <CounterApp /> 
