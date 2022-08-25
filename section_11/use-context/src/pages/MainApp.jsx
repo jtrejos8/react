7e4cd81008d92605a9d77dad4bf5dbbc824bd18d
jtrejos8 <jtrejos8.col@gmail.com>
2022-08-25 16:05:35 -0500

    add section 11 useContext

---
+++ section_11/use-context/src/pages/MainApp.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Navigate, Route, Routes, Link } from 'react-router-dom';
+import { AboutPage } from './AboutPage';
+import { HomePage } from './HomePage';
+import { LoginPage } from './LoginPage';
+import { Navbar } from '../components/Navbar';
+import { UserProvider } from '../context/UserProvider';
+
+export const MainApp = () => {
+  return (
+    <>
+      <UserProvider>
+        <Navbar />
+        <hr />
+
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='about' element={<AboutPage />} />
+          <Route path='login' element={<LoginPage />} />
+
+          {/* <Route path='/*' element={<LoginPage />} /> */}
+          <Route path='/*' element={<Navigate to="/about" />} />
+
+        </Routes>
+      </UserProvider>
+    </>
+  )
+};
